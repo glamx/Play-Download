427bdcb65894213bf0286886c0ce5f2fe1e07520
glamx <109116998+glamx@users.noreply.github.com>
2026-05-25 17:17:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
