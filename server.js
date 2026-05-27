3c99f3f8c3299f22b10c2ba3e9e0bc50b9255fd9
glamx <109116998+glamx@users.noreply.github.com>
2026-05-27 12:51:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -46,25 +46,21 @@ const videoId = new URL(videoUrl).searchParams.get("v");
       chunks.push(chunk);
     });
 
-    apiRes.on("end", function () {
+   apiRes.on("end", function () {
+  const body = Buffer.concat(chunks);
+  const data = JSON.parse(body.toString());
+  
+  console.log("Resposta da API:", JSON.stringify(data, null, 2)); // 👈 adicione isso temporariamente
 
-      const body = Buffer.concat(chunks);
+  const video = data.data || data; // tenta data.data primeiro, senão usa data direto
 
-      const data = JSON.parse(body.toString());
-
-      res.json({
-
-        title: data.title,
-
-        thumbnail: data.thumbnail,
-
-        channel: data.author,
-
-        duration: data.lengthSeconds
-
-      });
-
-    });
+  res.json({
+    title: video.title,
+    thumbnail: video.thumbnail,
+    channel: video.author,
+    duration: video.lengthSeconds
+  });
+});
 
   });
 
